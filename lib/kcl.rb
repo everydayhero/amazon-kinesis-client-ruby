b82e08b0ba6dc20e413006241f7e5cedab1f782b
soloman <soloman1124@gmail.com>
2014-11-18 23:46:56 +1000

    Add exector command builder

--- lib/kcl.rb
+++ lib/kcl.rb
@@ -1,2 +1,3 @@
 require 'kcl/version'
+require 'kcl/executor_command_builder'
 require 'kcl/record_processor'
---
+++ lib/kcl/executor_command_builder.rb
@@ -0,0 +1,30 @@
+module Kcl
+  class ExecutorCommandBuilder
+    def build
+      "#{java} -cp #{class_path} #{client_class} #{properties_file}"
+    end
+
+    private
+
+    def java
+      command = ENV.fetch('PATH_TO_JAVA', `which java`).strip
+      raise 'Missing JAVA PATH' if command.nil? || command.empty?
+
+      command
+    end
+
+    def client_class
+      "com.amazonaws.services.kinesis.multilang.MultiLangDaemon"
+    end
+
+    def class_path
+      jar_dir = File.expand_path '../../jars', __FILE__
+
+      Dir["#{jar_dir}/*.jar"].join ':'
+    end
+
+    def properties_file
+      File.expand_path '../../kcl.rb', __FILE__
+    end
+  end
+end
